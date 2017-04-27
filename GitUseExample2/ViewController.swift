d8c4935a8ed64337e33d074958645fc532654384
Klaus Dresbach <klaus.dresbach@t-online.de>
2017-04-27 20:16:04 +0200

    Initial Commit

---
+++ GitUseExample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample2
+//
+//  Created by Klaus Dresbach on 27.04.17.
+//  Copyright © 2017 Klaus Dresbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
